354d1f2d6585f67d14833bd9e2200e4690d571d1
Bonzow <joak.mail@gmail.com>
2016-07-17 18:35:40 -0400

    Programming Assignment 2: Lexical Scoping
    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# Assignment 1
+#Matrix inversion is usually a costly computation and there may be some 
+#benefit to caching the inverse of a matrix rather than compute it repeatedly.
 
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# 1 set value of matrix
+# 2 get value of the matrix
+# 3 set value of inverse of matrix
+# 4 get value of inverse of matrix
+#
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL }
+  get <- function() x
+  #
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)}
+#
+#Assignment 1.2
+# cacheSolve: This function computes the inverse of the special "matrix" returned 
+#by makeCacheMatrix above. If the inverse has already been calculated 
+#(and the matrix has not changed), then the cachesolve should retrieve the inverse 
+#from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data.")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m}
+
+# Results Check:
+# NOte that first run of casheSolve does not have the 'getting cache" note
+#
+#> x = makeCacheMatrix(iris[1:2, 1:2])
+#> x$get()
+#Sepal.Length Sepal.Width
+#1          5.1         3.5
+#2          4.9         3.0
+#> cacheSolve(x)
+#1         2
+#Sepal.Length -1.621622  1.891892
+#Sepal.Width   2.648649 -2.756757
+#> cacheSolve(x)
+#getting cached data.
+#1         2
+#Sepal.Length -1.621622  1.891892
+#Sepal.Width   2.648649 -2.756757
